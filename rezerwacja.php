15f61237ac83212d58ad99e2d882b3699f37816b
EwaSladkowa <sladkowayewa126@gmail.com>
2025-03-14 09:38:37 +0100

    .

--- rezerwacja.php
+++ rezerwacja.php
@@ -4,10 +4,25 @@
     $password="";
     $database="bazaa"
 
-    $connection= new mysqli(hostname: $serwer, username: $username, password: $password = "",database: $database);
+    $connection =
+        mysqli(hostname: $serwer, username: $username, password: $password = "",database: $database);
 
-    if ($connection -> connect_error){
-        die("Connection failed: " . $conn->connect_error);
+    if (!$connection ){
+        die(mysqli_error(mysql: $connection));
     }
-    echo "Connected successfully"
+    foreach($_POST as $key => $value){
+        echo $key, ":", $value;
+    }
+    $date = $_POST["data_rez"];
+    $phone = $_POST["telefon"];
+    $amountOfPeople = $_POST["ilosc_osob"];
+
+    mysqli_query(
+        $connection,
+        query: "INSERT INTO `rezerwacje`(`data_rez`, `liczba_osob`, `telefon`) 
+        VALUES ('$date','$amountOfPeople','$phone')" 
+    )
+    mysqli_close
+    
+
 ?>
